ff8a64f7267df7db3cd0e6dc0d768ccdfc0e987f
Daniel <daniel.castelline@gmail.com>
2021-03-02 23:51:30 -0600

    set up atlas and heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,9 +2,9 @@ const db = require('../models');
 const path = require('path');
 
 module.exports = (app) => {
-    // app.get("/", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../public/index.html"));
-    // });
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
     app.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
--- server.js
+++ server.js
@@ -12,10 +12,20 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
-
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, '/public')));
 // Connect Mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser: true});
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/dc-fitness-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // Require models
 const db = require('./models');
